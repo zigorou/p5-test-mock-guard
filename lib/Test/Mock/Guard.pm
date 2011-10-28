4545336d6b0a76a2c8eb2a07c7b01312b89d1058
Toru Yamaguchi <yamaguchi.toru@51547-mac.dena.local>
2011-10-28 13:58:57 +0900

    version++

--- lib/Test/Mock/Guard.pm
+++ lib/Test/Mock/Guard.pm
@@ -11,7 +11,7 @@ use Scalar::Util qw(blessed refaddr);
 use List::Util qw(max);
 use Carp qw(croak);
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 our @EXPORT = qw(mock_guard);
 
 sub mock_guard {
