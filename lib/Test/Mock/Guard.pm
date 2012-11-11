d5f2199e0c3870ddf920e84b89b81c3521f7ddf5
xaicron <xaicron@gmail.com>
2012-11-12 00:46:51 +0900

    Checking in changes prior to tagging of version 0.09.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 94bf8e6..7d7266b 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Test::Mock::Guard
    
    +0.09    Mon Nov 12 00:46:03 2012
    +        - repacking (no code changes)
    +
     0.08    Tue Jul  3 22:14:44 2012
             - Fixed a problem that when calling call_count() with un-mocked instance to throws exception. (xaicron)
             - keep prototype (thnks Belden Lyman)

--- lib/Test/Mock/Guard.pm
+++ lib/Test/Mock/Guard.pm
@@ -11,7 +11,7 @@ use Scalar::Util qw(blessed refaddr set_prototype);
 use List::Util qw(max);
 use Carp qw(croak);
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 our @EXPORT = qw(mock_guard);
 
 sub mock_guard {
